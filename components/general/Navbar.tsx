5c5206d8274651584029e88d8acdf3b2d6a55415
369webforall <369webforall@gmail.com>
2025-04-23 17:48:03 +0545

    imporve navbar

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { Bug } from "lucide-react";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+  const links = [
+    { id: 1, href: "/", label: "Home" },
+    { id: 2, href: "/dashboard", label: "Dashboard" },
+    { id: 3, href: "/issues", label: "Issues" },
+  ];
+
   return (
     <nav className="flex items-center gap-6 border-b border-gray-800 py-4">
       <Link href="/" className="flex gap-0.5 items-center font-bold">
@@ -10,30 +20,18 @@ const Navbar = () => {
       </Link>
 
       <ul className="flex items-center gap-4">
-        <li>
-          <Link
-            href="/"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/dashboard"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/issues"
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Issues
-          </Link>
-        </li>
+        {links.map((item) => (
+          <li key={item.id}>
+            <Link
+              href={item.href}
+              className={`${
+                currentPath === item.href ? "text-red-900" : "text-zinc-500"
+              } hover:text-zinc-800 transition-colors`}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
